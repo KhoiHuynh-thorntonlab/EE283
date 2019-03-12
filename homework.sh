055d4f6019bca57150b774397295f9a77d323cc9
KhoiHuynh-thorntonlab <srongthiencos@gmail.com>
2019-03-11 19:25:56 -0700

    Update homework.sh

--- homework.sh
+++ homework.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # to change name for atacseq
-for i in $(ls *R1.fq.gz);do ln -s $i $(grep $(echo $i |cut -f5 -d"_") README.ATACseq.txt | awk -v OFS="_" '{print "ATAC",$2,$3,$4,"R1"}'); done
-for i in $(ls *R2.fq.gz);do ln -s $i $(grep $(echo $i |cut -f5 -d"_") README.ATACseq.txt | awk -v OFS="_" '{print "ATAC",$2,$3,$4,"R2"}'); done
+for i in $(ls *R1.fq.gz);do mv $i $(grep $(echo $i |cut -f5 -d"_") README.ATACseq.txt | awk -v OFS="_" '{print "ATAC",$2,$3,$4,"R1"}'); done
+for i in $(ls *R2.fq.gz);do mv $i $(grep $(echo $i |cut -f5 -d"_") README.ATACseq.txt | awk -v OFS="_" '{print "ATAC",$2,$3,$4,"R2"}'); done
 
 
